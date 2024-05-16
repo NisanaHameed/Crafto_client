5f70857afb80c2ea79bf03bb8c37f7718ddea752
Nisana M <nisana686@gmail.com>
2024-05-16 15:23:21 +0530

    bug fixed

--- src/Services/Config/axios.ts
+++ src/Services/Config/axios.ts
@@ -1,8 +1,8 @@
 import axios,{AxiosInstance} from "axios";
 
 const Api:AxiosInstance = axios.create({
-    // baseURL:"https://www.crafto.live/api",
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://www.crafto.live/api",
+    // baseURL:"http://localhost:3000/api",
     withCredentials:true
 })
 
