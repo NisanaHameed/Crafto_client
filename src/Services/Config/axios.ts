a246ef052f46aeba2058bbf43f46992807a1525e
Nisana M <nisana686@gmail.com>
2024-05-20 16:29:30 +0530

    axios url changed

--- src/Services/Config/axios.ts
+++ src/Services/Config/axios.ts
@@ -2,7 +2,6 @@ import axios,{AxiosInstance} from "axios";
 
 const Api:AxiosInstance = axios.create({
     // baseURL:"https://www.crafto.live/api",
-    baseURL:'http://localhost:3000/api',
     withCredentials:true
 })
 
